7381c42cb4e121ffaaf1a3b8b395dacbd6dc7681
takuror25 <p-891622792@proton.me>
2025-10-12 22:39:32 +0900

    Update files.js

--- data/files.js
+++ data/files.js
@@ -1,1 +1,14 @@
 
+const files = [
+  { 
+    title: "2025-09-03 北海道・稚内市 山本太郎とおしゃべり会",
+    url: "talks/2025-09-03-wakkanai.html",
+    video: "https://www.youtube.com/live/3Lc0U6-RGYA"
+  },
+  { 
+    title: "2025-09-06 北海道・岩見沢市 山本太郎とおしゃべり会",
+    url: "talks/2025-09-06-iwamisawa.html",
+    video: "https://www.youtube.com/live/AbCdEfGhIjK"
+  },
+  // ← ここにどんどん追加していけます
+];
