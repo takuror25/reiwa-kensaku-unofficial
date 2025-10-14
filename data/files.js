f4724213db8936ab7fd9e6ccc10d7a58d1934872
takuror25 <p-891622792@proton.me>
2025-10-14 23:22:33 +0900

    Update files.js

--- data/files.js
+++ data/files.js
@@ -8,7 +8,7 @@ const files = [
  {
     title: "2025-09-07 北海道・北見市 山本太郎とおしゃべり会",
     url: "talks/2025-09-07-kitami.html",
-    video: "https://www.youtube.com/watch?v=QJhfxRkT4Bc
+    video: "https://www.youtube.com/watch?v=QJhfxRkT4Bc"
   },
 
  
